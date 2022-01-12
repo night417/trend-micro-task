f541c7341fc8db1a6b1e1f2e2b99ded22d38869f
night417 <65580161+night417@users.noreply.github.com>
2022-01-12 21:06:09 +0800

    Add staticText file

--- src/Pages/SignUp.tsx
+++ src/Pages/SignUp.tsx
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
 import { ReturnButton } from "../Components/ReturnButton";
+import { staticText } from "../util/staticText";
 
 export const SignUp = () => {
+  const { sign_up } = staticText;
+
   return (
     <div id="sign-up">
       <div className="sign-up__background">
         <div className="sign-up__content-box">
-          <ReturnButton className="sign-up__return-btn" text="Back" path="/" />
+          <ReturnButton
+            className="sign-up__return-btn"
+            text={sign_up.back}
+            path="/"
+          />
         </div>
       </div>
     </div>
---
+++ src/util/staticText.ts
@@ -0,0 +1,27 @@
+export const staticText = {
+  sign_up: {
+    back: "Back",
+    title: "Create an account",
+    sub_title: "Start from free",
+    separator: "Or use your email for registration",
+    comfirm:
+      "By creating account, you agree to accept our Privacy Policy, Terms of Service and Notification settings.",
+    btn_create: "Create an Free Account!",
+    alert_msg:
+      "Please accept the Privacy Policy,Terms of Service and Notification setting!",
+    login: {
+      text: "Already have an account?",
+      link: "Log in",
+    },
+  },
+  button: {
+    google: "Sign up with Google",
+    facebook: "Sign up with Facebook",
+  },
+  input: {
+    password: {
+      length: "8 Characters min.",
+      pattern: "One number",
+    },
+  },
+};
