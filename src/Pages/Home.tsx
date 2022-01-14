21745764cf3d495025464c372e6dea78d2ded774
night417 <65580161+night417@users.noreply.github.com>
2022-01-14 16:23:21 +0800

    Update Home page
    Add a Link to Sign-up page

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,5 +1,11 @@
 import React from "react";
+import { Link } from "../Components/Link";
 
 export const Home = () => {
-  return <h1>HOME PAGE</h1>;
+  return (
+    <>
+      <h1>HOME PAGE</h1>
+      <h1><Link text="GO TO SIGN-UP PAGE" to="/sign-up" /></h1>
+    </>
+  );
 };
